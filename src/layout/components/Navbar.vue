5907d922f30b27f1528dc3dc9976ad0356db4e0b
beevy <misshoon@126.com>
2023-11-11 11:05:50 +0800

    完成登出功能

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -27,7 +27,7 @@ export default {
   methods: {
     // 退出登录
     logout() {
-      this.$store.commit('setToken') // 清除cookie
+      this.$store.commit('user/setToken') // 清除cookie
       this.$router.push(`/login?redirect=${this.$route.fullPath}`)
     }
   }
