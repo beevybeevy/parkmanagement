2c52d155124319f4d97f2491803814b2831136bd
beevy <misshoon@126.com>
2023-11-11 12:01:27 +0800

    登陆时不同状态码的处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import { getToken } from './auth'
+import { Message } from 'element-ui'
+import router from '@/router'
 const service = axios.create({
   baseURL: 'https://api-hmzs.itheima.net/api',
   timeout: 5000 // request timeout
@@ -23,9 +25,22 @@ service.interceptors.request.use(
 // 响应拦截器
 service.interceptors.response.use(
   response => {
+    if (response.data.code !== 10000) {
+      // 1.跳转到登录页
+      Message.error(response.data.msg)
+      // 清除token信息
+      return Promise.reject(response.data)
+    }
     return response.data
   },
+  // 响应码不为200段
   error => {
+    // 失效后会得到401状态码
+    if (error.response?.status === 401) {
+      router.push('/login')
+      this.$store.commit('user/setToken')
+    }
+    Message.error(error.response?.data?.msg || '发送请求时出现了错误')
     return Promise.reject(error)
   }
 )
