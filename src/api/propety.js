85d67cbc75a632250a681343e7e1daf31bc1fe74
beevy <misshoon@126.com>
2023-11-24 10:44:30 +0800

    修改了不能校验表单中时间的bug

--- src/api/propety.js
+++ src/api/propety.js
@@ -4,3 +4,34 @@ import request from '@/utils/request'
 export function getAllPropetyListAPI(query) {
   return request.get('park/propertyfee', { params: query })
 }
+
+// 查询所有企业
+export function checkAllEnterpriseAPI() {
+  return request.get('park/all/enterprise')
+}
+// 查询所有楼宇
+export function checkAllBuildingAPI() {
+  return request.get('park/all/building')
+}
+// 查询物业费
+export function calculateFeeAPI(id, start, end) {
+  return request.post('park/propertyfee/pre/payment', {
+    buildingId: id,
+    startTime: start,
+    endTime: end })
+}
+
+// 添加账单
+export function addBillAPI(data) {
+  return request.post('park/propertyfee', data)
+}
+
+// 查询单个账单详情
+export function checkBillAIP(id) {
+  return request.get(`park/propertyfee/${id}`)
+}
+
+// 删除账单信息
+export function deleteBillAPI(id) {
+  return request.delete(`park/propertyfee/${id}`)
+}
--- src/views/Propety/index.vue
+++ src/views/Propety/index.vue
@@ -10,7 +10,7 @@
     </div>
     <!-- 表格区域 -->
     <div class="table">
-      <el-button type="primary">添加账单</el-button>
+      <el-button type="primary" @click="addBill">添加账单</el-button>
       <el-table v-loading="loading" style="width: 100%" :data="list">
         <el-table-column prop="index" label="序号" type="index" />
         <el-table-column label="账单编号" width="180" prop="billNumber" />
@@ -20,9 +20,9 @@
         <el-table-column label="账单金额(元)" prop="paymentAmount" />
         <el-table-column label="缴费时间" prop="createTime" />
         <el-table-column label="操作">
-          <template #default="scope">
-            <el-button size="mini" type="text">编辑</el-button>
-            <el-button size="mini" type="text">删除</el-button>
+          <template #default="{row}">
+            <el-button size="mini" type="text" @click="doCheck(row.id)">查看</el-button>
+            <el-button size="mini" type="text" @click="deleteBill(row.id)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -37,15 +37,92 @@
         @current-change="onCurrentChange"
       />
     </div>
+    <!-- 添加账单的弹窗 -->
+    <el-dialog title="添加账单" :visible.sync="dialogFormVisible">
+      <el-form :model="form" :rules="propetyRules">
+        <el-form-item label="选择租户" label-width="200px" prop="enterpriseId">
+          <el-select v-model="form.enterpriseId" placeholder="请选择租户">
+            <el-option v-for="item in enterpriseOptions" :key="item.id" :label="item.name" :value="item.id" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="租赁楼宇" label-width="200px" prop="buildingId">
+          <el-select v-model="form.buildingId" placeholder="请选择租赁楼宇">
+            <el-option v-for="item in buidingOptions" :key="item.id" :label="item.name" :value="item.id" />
+          </el-select>
+        </el-form-item>
+        <!-- 表单校验没有做完那，
+          清空日期后，需要提示选择缴费日期 -->
+        <el-form-item label="缴费周期" label-width="200px" prop="billTime">
+          <el-date-picker v-model="form.billTime" type="daterange" range-separator="-" start-placeholder="开始日期" end-placeholder="结束日期" value-format="yyyy-MM-dd" />
+        </el-form-item>
+        <el-form-item label="支付金额" label-width="200px" prop="paymentAmount">
+          <el-input
+            v-if="!!form.paymentAmount"
+            v-model="form.paymentAmount"
+            :disabled="true"
+          />
+          <el-input
+            v-else
+            v-model="form.paymentAmount"
+            placeholder="请输入支付金额"
+            :disabled="true"
+          />
+        </el-form-item>
+        <el-form-item label="支付方式" label-width="200px" prop="paymentMethod">
+          <el-select v-model="form.paymentMethod" placeholder="请选择租赁楼宇">
+            <el-option label="微信" :value="1" />
+            <el-option label="支付宝" :value="2" />
+            <el-option label="现金" :value="3" />
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="doSubmit">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 查看的弹窗 -->
+    <el-dialog title="添加账单" :visible.sync="showDialog">
+      <div class="wrapper">
+        <div class="wrapper-item">
+          <span>租户名称：</span><span>{{ billList.enterpriseName }}</span>
+        </div>
+        <div class="wrapper-item">
+          <span>租户楼宇：</span><span>{{ billList.buildingName }}</span>
+        </div>
+        <div class="wrapper-item">
+          <span>缴费周期：</span><span>{{ billList.startTime }}至{{ billList.endTime }}</span>
+        </div>
+        <div class="wrapper-item">
+          <span>物业费(元/m²)：</span><span>{{ billList.propertyFeePrice }}</span>
+        </div>
+        <div class="wrapper-item">
+          <span>账单金额(元)：</span><span>{{ billList.paymentAmount }}</span>
+        </div>
+        <div class="wrapper-item">
+          <span>支付方式：</span><span>{{ paymentMethodText }}</span>
+        </div>
+        <div class="wrapper-item">
+          <span>缴费时间：</span><span>{{ billList.createTime }}</span>
+        </div>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="showDialog = false">取 消</el-button>
+        <el-button type="primary" @click="showDialog = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { getAllPropetyListAPI } from '@/api/propety'
+import { getAllPropetyListAPI, checkAllEnterpriseAPI, checkAllBuildingAPI, calculateFeeAPI, addBillAPI, checkBillAIP, deleteBillAPI } from '@/api/propety'
+import { createRequiredRule } from '@/utils/validate'
 export default {
   name: 'Building',
   data() {
     return {
+      dialogFormVisible: false,
+      showDialog: false,
       list: [],
       query: {
         page: 1,
@@ -56,9 +133,45 @@ export default {
       },
       total: 0,
       loading: false,
-      time: ''
+      time: '',
+      form: {
+        enterpriseId: '',
+        buildingId: '',
+        startTime: '',
+        endTime: '',
+        paymentAmount: '',
+        // 付款方式1微信2支付宝3现金
+        paymentMethod: '',
+        billTime: []
+      },
+      enterpriseOptions: [],
+      buidingOptions: [],
+      billList: {},
+      propetyRules: {
+        enterpriseId: [createRequiredRule('必填项，不能为空')],
+        buildingId: [createRequiredRule('必填项，不能为空')],
+        paymentAmount: [createRequiredRule('必填项，不能为空')],
+        paymentMethod: [createRequiredRule('必填项，不能为空')]
+
+      }
     }
   },
+
+  computed: {
+    paymentMethodText() {
+      switch (this.billList.paymentMethod) {
+        case 1:
+          return '微信'
+        case 2:
+          return '支付宝'
+        case 3:
+          return '现金'
+        default:
+          return '未知支付方式'
+      }
+    }
+  },
+
   watch: {
     time(newValue, oldValue) {
       // 在time数据变化时触发的逻辑 如果清空之后需要更改start和end的值
@@ -66,11 +179,25 @@ export default {
         this.query.start = null
         this.query.end = null
       }
+    },
+    // 'form.xxx'可以监听对象中的属性，需要用单引号
+    form: {
+      deep: true,
+      async handler(newVal, oldVal) {
+        if (newVal.buildingId && newVal.billTime) {
+          const res = await calculateFeeAPI(newVal.buildingId, newVal.billTime[0], newVal.billTime[1])
+          // console.log(res)
+          this.form.paymentAmount = res.data
+        }
+      }
+
     }
   },
+
   created() {
     this.getAllPropetyList()
   },
+
   methods: {
     async getAllPropetyList() {
       this.loading = true
@@ -94,6 +221,43 @@ export default {
       this.getAllPropetyList(this.query)
       this.loading = false
       // console.log(this.query)
+    },
+    // 点击添加账单按钮后
+    async addBill() {
+      const res1 = await checkAllEnterpriseAPI()
+      const res2 = await checkAllBuildingAPI()
+      // console.log(res2)
+      this.enterpriseOptions = res1.data
+      this.buidingOptions = res2.data
+      this.dialogFormVisible = true
+    },
+    // handleBlur() {
+    //   this.form.startTime = this.billTime[0]
+    //   this.form.endTime = this.billTime[1]
+    //   // console.log(this.form)
+    // },
+    async doSubmit() {
+      this.form.startTime = this.form.billTime[0]
+      this.form.endTime = this.form.billTime[1]
+      delete this.form.billTime
+      await addBillAPI(this.form)
+      this.$message.success('添加账单成功')
+      this.dialogFormVisible = false
+      this.getAllPropetyList()
+    },
+    async doCheck(id) {
+      const res = await checkBillAIP(id)
+      console.log(res)
+      this.billList = res.data
+      this.showDialog = true
+    },
+    deleteBill(id) {
+      this.$confirm('确认删除帐单么').then(() => {
+        return deleteBillAPI(id)
+      }).then(() => {
+        this.$message.success('删除成功')
+        this.getAllPropetyList()
+      })
     }
   }
 }
@@ -119,5 +283,20 @@ export default {
     width: 220px;
     margin-right: 10px;
   }
+}
+
+.el-input{
+  width: 210px;
+}
+
+.wrapper{
+  width: 380px;
+  height: 274px;
+  margin-left:150px
+}
+
+.wrapper-item{
+  margin-bottom: 25px;
+  font-size: 14px;
 }
 </style>
